ecc8f852f32fce83569b7dfd15fda34bf55a4191
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-06-15 21:27:17 -0500

    not incorporating react-query

--- he-search-app/src/App.js
+++ he-search-app/src/App.js
@@ -1,23 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+
 import Search from './Components/Search/index';
 
 
 
-
-function Example() {
-  const { isLoading, error, data, isFetching } = useQuery("repoData", () =>
-    fetch(
-      "https://api.github.com/repos/tannerlinsley/react-query"
-    ).then((res) => res.json())
-  );
-
-  if (isLoading) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
-
 function App() {
+  
   return (
     <BrowserRouter>
       <Switch>
--- he-search-app/src/index.js
+++ he-search-app/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { QueryClientProvider, QueryClient } from 'react-query';
 import './index.css';
 import App from './App';
 
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
