defa0ad19773c84af643cd388e310d4ce172661a
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-06-15 22:52:38 -0500

    added dropdown for sort

--- he-search-app/src/Components/Search/Search.js
+++ he-search-app/src/Components/Search/Search.js
@@ -8,10 +8,19 @@ const Search = () => {
     const [query, setQuery] = useState("");
     const [filteredResults, setFilteredResults] = useState([]);
     const [error, setError] = useState("");
+    const [language, setLanguage] = useState("");
+    const [sort, setSort] = useState("");
+
+    const languages = ["JavaScript", "TypeScript", "HTML", "CSS", "Java", "Objective-C", "Python", "Ruby", "PHP", "C#", "C", "C++"];
    
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch(`https://api.github.com/search/repositories?q=${query}&sort=stars&order=desc`);
+        const queryString = encodeURIComponent(`${query}${language.length ? `+language:${language}` : ""}`);
+
+        const sortString = sort.length ? `&sort=stars&order=${sort}` : "" ;
+
+        const response = await fetch(`https://api.github.com/search/repositories?q=${queryString}${sortString}`);
+
         if (response.ok) {
             const data = await response.json();
             setFilteredResults(data.items);
@@ -28,9 +37,26 @@ return (
                     <input type='search' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Search' />
                 </div>
                 {error.length ? (<div className='search_error'>{error}</div>) : null}
-                <select>
-                    <option></option>
+                <select 
+                    className='search__dropdown-language'
+                    value={language}
+                    onChange={(e) => setLanguage(e.target.value)}>
+                        <option value=""></option>
+                    {languages.map((language) => {
+                        return (
+                            <option
+                                value={language} key={language}>{language}</option>
+                        )
+                    })}
                 </select>
+                <select className='search__dropdown-bestmatch'
+                    value={sort}
+                    onChange={(e) => setSort(e.target.value)}>
+                        <option value="">Sort: Best match</option>
+                        <option value="desc">Sort: Most stars</option>
+                        <option value="asc">Sort: Least stars</option>
+                </select>
+                <button className='search__submit' type='submit'>Submit</button>
             </form>
         </div>
         <div className='search__results-container'>
