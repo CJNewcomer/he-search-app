485e31c7e5bfa95b98752f325240d84ca7b89122
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-06-15 22:12:35 -0500

    working search function with results

--- he-search-app/src/Components/Search/Search.js
+++ he-search-app/src/Components/Search/Search.js
@@ -1,32 +1,52 @@
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 import './Search.css';
 
 const Search = () => {
     const [query, setQuery] = useState("");
+    const [filteredResults, setFilteredResults] = useState([]);
+    const [error, setError] = useState("");
    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch(`https://api.github.com/search/repositories?q=${query}&sort=stars&order=desc`);
+        if (response.ok) {
+            const data = await response.json();
+            setFilteredResults(data.items);
+        } else {
+            setError("Something went wrong, please try again...");
+        } 
+    };
 
 return (
     <>
         <div className='search__container'>
-            <div className='search__input'>
-                <input value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Search or jump to...' />
-            </div>
+            <form className='search__form' onSubmit={handleSubmit}>
+                <div className='search__input'>
+                    <input type='search' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Search' />
+                </div>
+                {error.length ? (<div className='search_error'>{error}</div>) : null}
+                <select>
+                    <option></option>
+                </select>
+            </form>
         </div>
         <div className='search__results-container'>
             {filteredResults.map((repository) => {
-                const { id, name, description, starredNumber, language, owner } = repository;
+                const { id, name, description, stargazers_count, language, owner:{ login } } = repository;
                 return (
                     <div className='search__results' key={id}>
-                        
                             <div className='repo__container'>
                                 <div className='repo__container-info'>
+                                <NavLink to={`/${login}/${name}`}>
                                     <h3>{name}</h3>
+                                </NavLink>
                                     <h3>{description}</h3>
-                                    <h3>{starredNumber}</h3>
+                                    <h3>{stargazers_count}</h3>
                                     <h3>{language}</h3>
-                                    <h3>{owner}</h3>
+                                    <h3>{login}</h3>
                                 </div>
                             </div>
                     </div>
